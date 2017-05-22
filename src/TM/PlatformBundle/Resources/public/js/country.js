bf6ff773147e5c45802b2cb198857b2dac90609d
Pierre-Lefevre <lefevre.pierre.m.d@gmail.com>
2017-05-22 18:54:48 +0200

    Correction du script pour afficher les drapeaux

--- src/TM/PlatformBundle/Resources/public/js/country.js
+++ src/TM/PlatformBundle/Resources/public/js/country.js
@@ -1,30 +1,29 @@
 function addFlags() {
+    var country    = ["HR", "TW", "MS", "CZ", "VC", "AD", "EH", "HK", "GA", "CATALONIA", "SOMALILAND", "MU", "KZ", "PW", "AN", "BG", "NF", "GS", "MM", "TM", "DM", "SM", "OM", "CK", "YT", "RW", "SG", "AL", "EC", "ET", "BM", "GM", "AW", "IT", "BR", "KG", "SB", "EE", "AO", "TR", "BN", "GU", "TK", "BS", "FR", "LY", "MK", "PY", "ST", "PN", "PH", "BB", "TN", "SK", "ID", "MP", "NC", "WS", "FK", "KP", "EG", "SN", "TIBET", "KY", "IM", "NO", "MZ", "KW", "AG", "SR", "EU", "JE", "PE", "BT", "SC", "NG", "JO", "GD", "CD", "SO", "DE", "VA", "SL", "PA", "IQ", "IE", "GW", "VG", "TZ", "MX", "UM", "CV", "AS", "DJ", "MN", "ZANZIBAR", "DO", "MY", "AT", "ML", "CA", "BH", "RU", "CF", "HM", "AZ", "MA", "PM", "CU", "PT", "BD", "TL", "ENGLAND", "TO", "DK", "NA", "CO", "GY", "KURDISTAN", "MH", "QA", "PF", "DZ", "LI", "FJ", "VU", "LB", "MQ", "GE", "AI", "BV", "MR", "MC", "LK", "BJ", "FM", "XK", "GR", "TG", "ZM", "CY", "PR", "AF", "GF", "IS", "MD", "KR", "GP", "IL", "RO", "ZA", "CN", "MV", "LS", "GT", "SS", "BO", "TD", "TH", "HU", "ER", "TC", "SD", "HN", "MG", "PL", "WALES", "AM", "BE", "KE", "AR", "NI", "HT", "IO", "GH", "NU", "MT", "KH", "CG", "CL", "VN", "SCOTLAND", "FI", "RE", "NP", "BA", "LT", "WF", "MO", "GN", "SZ", "TT", "ES", "UZ", "CH", "PG", "LU", "SA", "IR", "UG", "US", "VI", "SX", "YE", "NZ", "JP", "GQ", "PK", "BF", "SY", "CR", "TF", "SV", "PS", "AU", "LA", "MW", "NL", "GB", "NR", "GL", "RS", "CM", "GI", "KM", "TV", "ME", "CW", "GG", "JM", "AE", "SI", "TJ", "BZ", "VE", "IN", "SH", "KI", "ZW", "IC", "BY", "FO", "NE", "UY", "LR", "CI", "LC", "SE", "KN", "LV", "BW", "UA"];
     var notCountry = ["EZ", "UN"];
-    var options    = document.querySelectorAll(".form-add-edit .second-column select:nth-of-type(2) option, #travel_search_countries option, .countries > ul > li");
+    var elements    = document.querySelectorAll(".form-add-edit .second-column select:nth-of-type(2) option, #travel_search_countries option, .countries > ul > li");
 
-    for (var i = 0; i < options.length; i++) {
+    for (var i = 0; i < elements.length; i++) {
 
-        var code = options[i].hasAttribute("data-code") ? options[i].getAttribute("data-code") : options[i].value;
+        var code = elements[i].hasAttribute("data-code") ? elements[i].getAttribute("data-code") : elements[i].value;
 
         if (notCountry.indexOf(code) !== -1) {
-            options[i].remove();
+            elements[i].remove();
             continue;
         }
 
-        if (code.length > 0) {
-            var div = document.createElement("div");
+        var div = document.createElement("div");
 
+        if (country.indexOf(code.toUpperCase()) !== -1) {
             div.classList.add("flag");
             div.classList.add("flag-" + code.toLowerCase());
-            if (window.getComputedStyle(div).getPropertyValue("background-position") === "0% 0%") {
-                div.classList.remove("flag");
-                div.classList.remove("flag-" + code.toLowerCase());
-                if (!options[i].hasAttribute("data-flag-not-empty")) {
-                    div.classList.add("empty-flag");
-                }
-            }
+            elements[i].insertBefore(div, elements[i].firstChild);
+            continue;
+        }
 
-            options[i].insertBefore(div, options[i].firstChild);
+        if (!elements[i].hasAttribute("data-flag-not-empty")) {
+            div.classList.add("empty-flag");
+            elements[i].insertBefore(div, elements[i].firstChild);
         }
     }
 }
